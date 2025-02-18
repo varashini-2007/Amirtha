b349e62c232c7cbafc3cedafd1232469b17204eb
Amirtha27 <namirthavarshini347@gmail.com>
2025-02-18 16:03:08 +0530

    Add files via upload

---
+++ kilometer to meter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+        float km,meter;
+        printf("enter the kilometer:");
+        scanf("%f",&km);
+        meter=km*1000;
+        printf("meter: %.2f\n",meter);
+        return 0;
+
+}
